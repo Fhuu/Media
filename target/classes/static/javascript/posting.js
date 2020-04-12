e9c32619d165481513a312715c66b51cedefa666
Fhuu <dirobaloska@outlook.com>
2020-04-12 18:59:38 +0200

    added post feature with mysql Query

--- target/classes/static/javascript/posting.js
+++ target/classes/static/javascript/posting.js
@@ -1,13 +1,50 @@
 "use strict";
 
-document.getElementById("post-write").onclick = function() {
-    var xhr = new XMLHttpRequestRequest();
-    xhr.open("POST", "/post/newpost", true);
-    xhr.setRequestHeader("Content-Type", "application/json;UTF-8");
-    xhr.send(JSON.stringify(
-        {
-            
+var username;
+var loginStatus;
+
+var loadPosts = function() {
+    username = sessionStorage.getItem("username");
+    loginStatus = sessionStorage.getItem("loginStatus");
+    console.log(username);
+    console.log(loginStatus);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/post/viewall", true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            console.log(this.responseText);
+            var results= JSON.parse(this.responseText);
+            let write = "";
+            for(let index = 0; index < results.length; index++) {
+                write = write + "<div class='post'>" +
+                    "<span class=\"username-holder\">" + results[index].username + "</span>" +
+                    "<span class=\"content-holder\">" + results[index].content + "</span>" +
+                    "</div>";
+            }
+            document.getElementById("post-container").innerHTML = document.getElementById("post-container").innerHTML + write;
         }
-    ))
+    }
+}
+
+document.getElementById("post-write").onclick = function() {
+    console.log("publish button clicked");
+    var xhrPost = new XMLHttpRequest();
+    xhrPost.open("POST", "/post/add", true);
+    xhrPost.setRequestHeader("Content-Type", "application/json;UTF-8");
+    var content = document.getElementById("write-post").value;
+    if(content.replace(/\s+/g,"") != "") {
+        xhrPost.send(JSON.stringify(
+            {
+                "content" : content ,
+                "username" : username
+            }
+        ))
+    }
+    xhrPost.onreadystatechange = function () {
+        if(xhrPost.status == 200 && xhrPost.readyState == 4) {
+            window.location.reload(true);
+        }
+    }
     
 }
